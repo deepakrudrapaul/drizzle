e50e0ac1a6fe5698342e9931089f57df7077dd2e
Deepak Paul <rudra.ad7@gmail.com>
2023-06-26 23:38:29 +0530

    feat: Added constants and types

--- constants/index.ts
+++ constants/index.ts
@@ -1,32 +1,45 @@
 export const NavLinks = [
-    {href: '/', key: 'Inspiration', text: 'Inspiration'},
-    {href: '/', key: 'Find Projects', text: 'Find Projects'},
-    {href: '/', key: 'Learn Development', text: 'Learn Development'},
-    {href: '/', key: 'Career Advancement', text: 'Career Advancement'},
-    {href: '/', key: 'Hire Developers', text: 'Hire Developers'}
-
-]
-
+  { href: "/", key: "Inspiration", text: "Inspiration" },
+  { href: "/", key: "Find Projects", text: "Find Projects" },
+  { href: "/", key: "Learn Development", text: "Learn Development" },
+  { href: "/", key: "Career Advancement", text: "Career Advancement" },
+  { href: "/", key: "Hire Developers", text: "Hire Developers" },
+];
 
 export const footerLinks = [
-    {
-        title: 'For Developers',
-        links: [
-            'Go Pro',
-            'Development Blog',
-            'Open Source Projects',
-            'Refer a Friend',
-            'Code of Conduct'
-        ]
-    },
-    {
-        title: 'Hire Developers',
-        links: [
-            'Post Job Opening',
-            'Post Freelance Project',
-            'Search For Developers',
-            'Refer a Friend',
-            'Code of Conduct'
-        ]
-    }
-]
+  {
+    title: "For Developers",
+    links: [
+      "Go Pro",
+      "Development Blog",
+      "Open Source Projects",
+      "Refer a Friend",
+      "Code of Conduct",
+    ],
+  },
+  {
+    title: "Hire Developers",
+    links: [
+      "Post Job Opening",
+      "Post Freelance Project",
+      "Search For Developers",
+      "Refer a Friend",
+      "Code of Conduct",
+    ],
+  },
+];
+
+export const categoryFilters = [
+  "Frontend",
+  "Backend",
+  "Full Stack",
+  "Mobile",
+  "UI/UX",
+  "Game Development",
+  "Dev Ops",
+  "Data Science",
+  "Machine Learning",
+  "Blockchain",
+  "E-commerce",
+  "Chatbots",
+];
